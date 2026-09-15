d8d12c1807765866b2751cf6e31e8ff724b5b017
jasminagar <jasminagar21@gmail.com>
2026-09-15 19:00:31 +0200

    Saves entities to database. Tested in Main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,62 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import config.HibernateConfig;
 import dto.MovieDTO;
 import dto.MovieResponseDTO;
+import entities.Movie;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import service.ApiReader;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+        EntityManager em = emf.createEntityManager();
 
-        ApiReader apiReader = new ApiReader();
-        String json = apiReader.getAllDataFromApi();
+        Movie movie = new Movie();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
+        movie.setId(223456L);
+        movie.setTitle("Test Movie");
+        movie.setReleaseDate(LocalDate.of(2025, 1, 1));
+        movie.setVoteAverage(8.5);
+        movie.setPopularity(100.0);
 
-        try {
+        em.getTransaction().begin();
 
-            MovieResponseDTO movieResponseDTO =
-                    objectMapper.readValue(json, MovieResponseDTO.class);
+        em.persist(movie);
 
-            for(MovieDTO movie : movieResponseDTO.getResults()){
-                System.out.println("ID: " + movie.getId());
-                System.out.println("Title: " + movie.getTitle());
-                System.out.println("Release date: " + movie.getReleaseDate());
-                System.out.println("Rating: " + movie.getVoteAverage());
-                System.out.println("Popularity: " + movie.getPopularity());
-                System.out.println("Genres: " + movie.getGenreIds());
-                System.out.println("-------------------------");
-            }
+        em.getTransaction().commit();
 
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        em.close();
+        emf.close();
+
+
+//        ApiReader apiReader = new ApiReader();
+//        String json = apiReader.getAllDataFromApi();
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.registerModule(new JavaTimeModule());
+//
+//        try {
+//
+//            MovieResponseDTO movieResponseDTO =
+//                    objectMapper.readValue(json, MovieResponseDTO.class);
+//
+//            for(MovieDTO movie : movieResponseDTO.getResults()){
+//                System.out.println("ID: " + movie.getId());
+//                System.out.println("Title: " + movie.getTitle());
+//                System.out.println("Release date: " + movie.getReleaseDate());
+//                System.out.println("Rating: " + movie.getVoteAverage());
+//                System.out.println("Popularity: " + movie.getPopularity());
+//                System.out.println("Genres: " + movie.getGenreIds());
+//                System.out.println("-------------------------");
+//            }
+//
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
