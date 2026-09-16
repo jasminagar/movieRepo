f0f2feca1b97777fe617452a4147a450c95bbfd7
Melissabekirova04 <229076304+Melissabekirova04@users.noreply.github.com>
2026-09-16 09:56:58 +0200

    Add Genre entity with movie relationship

---
+++ src/main/java/entities/Genre.java
@@ -0,0 +1,24 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "genres")
+    private List<Movie> movies = new ArrayList<>();
+}
