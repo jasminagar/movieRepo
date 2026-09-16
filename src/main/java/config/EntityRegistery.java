41af0aaa302ebdc85c77a5bf30e7711de99cd058
Melissabekirova04 <229076304+Melissabekirova04@users.noreply.github.com>
2026-09-16 09:58:02 +0200

    Register all movie entities in Hibernate

--- src/main/java/config/EntityRegistery.java
+++ src/main/java/config/EntityRegistery.java
@@ -1,13 +1,17 @@
 package config;
 
+import entities.Actor;
+import entities.Director;
+import entities.Genre;
 import entities.Movie;
 import org.hibernate.cfg.Configuration;
 
 public class EntityRegistery {
-static void registerEntities(Configuration configuration){
-    configuration.addAnnotatedClass(Movie.class);
 
-}
-
-
+    static void registerEntities(Configuration configuration) {
+        configuration.addAnnotatedClass(Movie.class);
+        configuration.addAnnotatedClass(Actor.class);
+        configuration.addAnnotatedClass(Director.class);
+        configuration.addAnnotatedClass(Genre.class);
+    }
 }
