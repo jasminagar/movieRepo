d00944ee8ee1cde1a8568c50d9a0586a75aebce4
Melissabekirova04 <229076304+Melissabekirova04@users.noreply.github.com>
2026-09-17 09:47:17 +0200

    Update tests for movie CRUD operations
    Test that movies can be created, retrieved, updated, and deleted correctly. Verify that the update operation changes the title and release date while keeping the other fields unchanged.

--- src/test/java/dao/MoviedaoTest.java
+++ src/test/java/dao/MoviedaoTest.java
@@ -21,6 +21,7 @@ public class MoviedaoTest {
 
     @BeforeAll
     static void setUpEmf() {
+
         emf = HibernateConfig.getEntityManagerFactory();
 
         EntityManager em = emf.createEntityManager();
@@ -28,9 +29,12 @@ public class MoviedaoTest {
         try {
             em.getTransaction().begin();
 
-            em.createQuery("DELETE FROM Movie").executeUpdate();
+            em.createQuery(
+                    "DELETE FROM Movie"
+            ).executeUpdate();
 
             em.getTransaction().commit();
+
         } finally {
             em.close();
         }
@@ -53,19 +57,43 @@ public class MoviedaoTest {
 
         movie.setId(999991L);
         movie.setTitle("Test Movie");
-        movie.setReleaseDate(LocalDate.of(2025, 1, 1));
+        movie.setReleaseDate(
+                LocalDate.of(2025, 1, 1)
+        );
         movie.setVoteAverage(8.5);
         movie.setPopularity(100.0);
 
         moviedao.saveMovie(movie);
 
-        Movie savedMovie = moviedao.findMovieById(999991L);
+        Movie savedMovie =
+                moviedao.findMovieById(999991L);
 
         assertNotNull(savedMovie);
-        assertEquals(Long.valueOf(999991L), savedMovie.getId());
-        assertEquals("Test Movie", savedMovie.getTitle());
-        assertEquals(8.5, savedMovie.getVoteAverage());
-        assertEquals(100.0, savedMovie.getPopularity());
+
+        assertEquals(
+                Long.valueOf(999991L),
+                savedMovie.getId()
+        );
+
+        assertEquals(
+                "Test Movie",
+                savedMovie.getTitle()
+        );
+
+        assertEquals(
+                LocalDate.of(2025, 1, 1),
+                savedMovie.getReleaseDate()
+        );
+
+        assertEquals(
+                8.5,
+                savedMovie.getVoteAverage()
+        );
+
+        assertEquals(
+                100.0,
+                savedMovie.getPopularity()
+        );
 
         moviedao.deleteMovie(999991L);
     }
@@ -77,17 +105,28 @@ public class MoviedaoTest {
 
         movie.setId(999992L);
         movie.setTitle("Find Test");
-        movie.setReleaseDate(LocalDate.of(2024, 5, 10));
+        movie.setReleaseDate(
+                LocalDate.of(2024, 5, 10)
+        );
         movie.setVoteAverage(7.5);
         movie.setPopularity(80.0);
 
         moviedao.saveMovie(movie);
 
-        Movie foundMovie = moviedao.findMovieById(999992L);
+        Movie foundMovie =
+                moviedao.findMovieById(999992L);
 
         assertNotNull(foundMovie);
-        assertEquals(Long.valueOf(999992L), foundMovie.getId());
-        assertEquals("Find Test", foundMovie.getTitle());
+
+        assertEquals(
+                Long.valueOf(999992L),
+                foundMovie.getId()
+        );
+
+        assertEquals(
+                "Find Test",
+                foundMovie.getTitle()
+        );
 
         moviedao.deleteMovie(999992L);
     }
@@ -99,7 +138,9 @@ public class MoviedaoTest {
 
         movie1.setId(999993L);
         movie1.setTitle("Movie One");
-        movie1.setReleaseDate(LocalDate.of(2024, 1, 1));
+        movie1.setReleaseDate(
+                LocalDate.of(2024, 1, 1)
+        );
         movie1.setVoteAverage(7.0);
         movie1.setPopularity(50.0);
 
@@ -107,14 +148,17 @@ public class MoviedaoTest {
 
         movie2.setId(999994L);
         movie2.setTitle("Movie Two");
-        movie2.setReleaseDate(LocalDate.of(2024, 2, 1));
+        movie2.setReleaseDate(
+                LocalDate.of(2024, 2, 1)
+        );
         movie2.setVoteAverage(8.0);
         movie2.setPopularity(60.0);
 
         moviedao.saveMovie(movie1);
         moviedao.saveMovie(movie2);
 
-        List<Movie> movies = moviedao.findAllMovies();
+        List<Movie> movies =
+                moviedao.findAllMovies();
 
         assertNotNull(movies);
         assertTrue(movies.size() >= 2);
@@ -124,13 +168,15 @@ public class MoviedaoTest {
     }
 
     @Test
-    void updateMovieShouldUpdateMovie() {
+    void updateMovieShouldUpdateTitleAndReleaseDate() {
 
         Movie movie = new Movie();
 
         movie.setId(999995L);
         movie.setTitle("Old Title");
-        movie.setReleaseDate(LocalDate.of(2023, 1, 1));
+        movie.setReleaseDate(
+                LocalDate.of(2023, 1, 1)
+        );
         movie.setVoteAverage(6.0);
         movie.setPopularity(40.0);
 
@@ -139,18 +185,34 @@ public class MoviedaoTest {
         moviedao.updateMovie(
                 999995L,
                 "New Title",
-                LocalDate.of(2025, 1, 1),
-                9.0,
-                150.0
+                LocalDate.of(2025, 1, 1)
         );
 
-        Movie updatedMovie = moviedao.findMovieById(999995L);
+        Movie updatedMovie =
+                moviedao.findMovieById(999995L);
 
         assertNotNull(updatedMovie);
-        assertEquals("New Title", updatedMovie.getTitle());
-        assertEquals(LocalDate.of(2025, 1, 1), updatedMovie.getReleaseDate());
-        assertEquals(9.0, updatedMovie.getVoteAverage());
-        assertEquals(150.0, updatedMovie.getPopularity());
+
+        assertEquals(
+                "New Title",
+                updatedMovie.getTitle()
+        );
+
+        assertEquals(
+                LocalDate.of(2025, 1, 1),
+                updatedMovie.getReleaseDate()
+        );
+
+        // Disse værdier skal forblive uændrede
+        assertEquals(
+                6.0,
+                updatedMovie.getVoteAverage()
+        );
+
+        assertEquals(
+                40.0,
+                updatedMovie.getPopularity()
+        );
 
         moviedao.deleteMovie(999995L);
     }
@@ -162,19 +224,23 @@ public class MoviedaoTest {
 
         movie.setId(999996L);
         movie.setTitle("Delete Test");
-        movie.setReleaseDate(LocalDate.of(2022, 1, 1));
+        movie.setReleaseDate(
+                LocalDate.of(2022, 1, 1)
+        );
         movie.setVoteAverage(5.0);
         movie.setPopularity(20.0);
 
         moviedao.saveMovie(movie);
 
-        Movie savedMovie = moviedao.findMovieById(999996L);
+        Movie savedMovie =
+                moviedao.findMovieById(999996L);
 
         assertNotNull(savedMovie);
 
         moviedao.deleteMovie(999996L);
 
-        Movie deletedMovie = moviedao.findMovieById(999996L);
+        Movie deletedMovie =
+                moviedao.findMovieById(999996L);
 
         assertNull(deletedMovie);
     }
